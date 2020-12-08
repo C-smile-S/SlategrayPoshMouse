4b2a3c17e5591e446832f96fc1eefceb6f1496e1
Michael Nwogu <mikedbchi@gmail.com>
2020-12-08 14:25:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+/*var person 
+=
+ {
+  name
+:
+ "John", age
+:
+ 50
+};
+alert(
+person.name
+ + " is " + 
+person.age
+);
+
+// METHODS
+var txt = "Hello";
+var x = txt.replace("Hello", "Welcome");
+console.log(x);
+
+x = x.toUpperCase(); //pls note that methods to me looks like a function so "()" follows each methods
+console.log(x);
+x = x.toLocaleLowerCase();
+
+// Objects
+var coded = {
+  name:"Michael", age:28, Gender:"male"
+};
+console.log(coded.Gender);
+
+// ARRAYS
+
+let cars = [
+  "volvo", "ford", "benz"];
+  console.log(cars);
+  cars[0] = "farrari"
+  console.log(cars[0]);
+  document.write(cars.length);//You'll see 3 on the page
+cars.pop();
+console.log(cars); //Now its only left witb 2 strings there is also push methods for adding in the variable and among others
+cars.push("peugeot");
+console.log(cars); //peugeotis added in the console
+cars.sort();
+console.log(cars);
+
+//dates
+
+var d = new Date();
+document.write(d);
+var year = d.getFullYear();
+document.write(year);
+var month = d.getMonth();
+console.log(month);
+
+//Js math
+var mth = Math.random();
+alert(mth);
+
+// For loop
+var fruits = ["Apple", "Banana", "Orange"];
+for (x 
+of
+ 
+fruits
+) {
+  console.log(x);
+}
+
+// While loop
+var i = 0;
+while
+ (i 
+<
+ 10) {
+  console.log(i);
+  i++
+}*/
+$(function() {
+    var val = $(".a").text("Michael");
+    alert(val);
+});
+
+
+
+
+text()
+html() 
+val() 
+attr() 
+removeAttr()
